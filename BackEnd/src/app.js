796406b7dafd5ee11ca04cafe5e0b322634fe3de
Vraj Patel <vraj24092005@gmail.com>
2026-04-28 12:19:50 +0530

    change in cors

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: ["https://codeai-frontend.vercel.app", "http://localhost:5173"],
     credentials: true
 }))
 
